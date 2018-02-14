6ed1fccf530e0ed3423d4437d000971e9c31f78c
Kenichi Saito <hajime@studiohff.net>
2018-02-14 23:12:34 +0900

    vue-loaderのoptions.esModuleはデフォルトでtrueなので削除

--- webpack.config.js
+++ webpack.config.js
@@ -59,9 +59,6 @@ module.exports = [
                 {
                     test: /\.vue$/,
                     loader: "vue-loader",
-                    options: {
-                        esModule: true
-                    }
                 }
             ]
         },
