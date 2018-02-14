a7e854cbd6b7452cd630f99d1bf7f934dacfef22
Kenichi Saito <hajime@studiohff.net>
2018-02-14 23:30:49 +0900

    webpack.config.jsのリファクタリング

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,35 @@
 const path = require("path");
 
-const resolveConfig = {
-    extensions: [".js", ".ts"],
-    alias: {
-        "vue$": "vue/dist/vue.esm.js",
-        "vuex$": "vuex/dist/vuex.esm.js"
+module.exports = {
+    entry: {
+        basic: "./src/basic.ts",
+        vuex: "./src/vuex.ts",
+        sfc: "./src/sfc/main.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+        alias: {
+            "vue$": "vue/dist/vue.esm.js",
+            "vuex$": "vuex/dist/vuex.esm.js"
+        }
     }
 };
-
-module.exports = [
-    {
-        entry: "./src/basic.ts",
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "basic.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: "ts-loader"
-                }
-            ]
-        },
-        resolve: resolveConfig
-    },
-    {
-        entry: "./src/vuex.ts",
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "vuex.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: "ts-loader"
-                }
-            ]
-        },
-        resolve: resolveConfig
-    },
-    {
-        entry: "./src/sfc/main.ts",
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "sfc.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: "ts-loader",
-                    options: {
-                        appendTsSuffixTo: [/\.vue$/]
-                    }
-                },
-                {
-                    test: /\.vue$/,
-                    loader: "vue-loader",
-                }
-            ]
-        },
-        resolve: resolveConfig
-    },
-];
